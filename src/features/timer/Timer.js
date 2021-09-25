8615d465ebea4e69e4f7cb57aa4d10a0ae15800d
KamileSeidu <kseidu005@st.eg.edu.gh>
2021-09-25 18:06:56 +0000

    initial Entry

---
+++ src/features/timer/Timer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export const Timer = () => {
+    return (
+        <view>
+            <Text> Timer goes here</Text>
+        </view>
+    )
+
+}
+
+const styles = StyleSheet.create({
+
+
+})
